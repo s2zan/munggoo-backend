fad7b1b2c3d044100efd24705dcdde2fc61d2a0e
jo-bata <jobata@skuniv.ac.kr>
2019-06-29 20:53:21 +0900

    Initialize project

---
+++ src/main/java/com/mashup/munggoo/MunggooApplication.java
@@ -0,0 +1,13 @@
+package com.mashup.munggoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunggooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MunggooApplication.class, args);
+    }
+
+}
