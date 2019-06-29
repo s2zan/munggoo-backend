df6adc97d304cd6400bd3dae8e5d59cd7b395084
jo-bata <jobata@skuniv.ac.kr>
2019-06-30 05:17:42 +0900

    [#6] [ADD] device 테이블 맵핑

---
+++ src/main/java/com/mashup/munggoo/device/DeviceDto.java
@@ -0,0 +1,12 @@
+package com.mashup.munggoo.device;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class DeviceDto {
+    private String deviceKey;
+}
