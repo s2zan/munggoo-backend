669953d875c04f43ee4d82c2da38c5ec3ea133d4
jo-bata <jobata@skuniv.ac.kr>
2019-07-11 04:33:27 +0900

    [#14] [FIX] Device Entity 테스트 수정

--- src/test/java/com/mashup/munggoo/Device/DeviceTest.java
+++ src/test/java/com/mashup/munggoo/Device/DeviceTest.java
@@ -3,26 +3,11 @@ package com.mashup.munggoo.Device;
 import com.mashup.munggoo.device.Device;
 import com.mashup.munggoo.device.ReqDeviceDto;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
-@DataJpaTest
 public class DeviceTest {
-    @Autowired
-    private TestEntityManager testEntityManager;
-
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
-
     private Device device;
 
     private ReqDeviceDto reqDeviceDto;
@@ -34,8 +19,7 @@ public class DeviceTest {
     }
 
     @Test
-    public void saveDevice() {
-        Device savedDevice = this.testEntityManager.persistAndFlush(device);
-        assertThat(savedDevice.getDeviceKey()).isEqualTo("qwer1234");
+    public void constructDevice() {
+        assertThat(device.getDeviceKey()).isEqualTo("qwer1234");
     }
 }
