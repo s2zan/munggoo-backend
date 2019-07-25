9280fda54da9e6b7e5de1d0b83913a9c85b044c7
Lee Jian <209rrsz@gmail.com>
2019-07-25 12:40:38 +0900

    [#23] [FIX] GeneratedQuizDto -> GeneratedQuiz

--- src/main/java/com/mashup/munggoo/quiz/GeneratedQuiz.java
+++ src/main/java/com/mashup/munggoo/quiz/GeneratedQuiz.java
@@ -5,11 +5,11 @@ import kr.co.shineware.nlp.komoran.model.Token;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GeneratedQuizDto {
+public class GeneratedQuiz {
     public List<Token> selected;
     public List<Token> preliminary;
 
-    public GeneratedQuizDto(){
+    public GeneratedQuiz(){
         selected = new ArrayList<Token>();
         preliminary = new ArrayList<Token>();
     }
