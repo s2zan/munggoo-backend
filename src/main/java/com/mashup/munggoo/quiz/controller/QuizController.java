96e44c77266af70ca89ec0a07779fd01f56d0545
jo-bata <jobata@skuniv.ac.kr>
2019-08-10 12:02:22 +0900

    [#46] [FIX] 불필요한 부분 삭제

--- src/main/java/com/mashup/munggoo/quiz/controller/QuizController.java
+++ src/main/java/com/mashup/munggoo/quiz/controller/QuizController.java
@@ -2,23 +2,18 @@ package com.mashup.munggoo.quiz.controller;
 
 import com.mashup.munggoo.quiz.dto.ReqAnswersDto;
 import com.mashup.munggoo.quiz.dto.ResQuizzesDto;
-import com.mashup.munggoo.quiz.service.HighlightForQuizService;
 import com.mashup.munggoo.quiz.service.QuizService;
-import com.mashup.munggoo.quiz.dto.ReqAnswerDto;
 import com.mashup.munggoo.quiz.dto.ScoreDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/v1/devices/{device-id}/files/{file-id}/quiz")
 public class QuizController {
     private final QuizService quizService;
-    private final HighlightForQuizService highlightForQuizService;
 
     @GetMapping
     public ResponseEntity<ResQuizzesDto> createQuiz(@PathVariable(value="device-id") Long deviceId,
